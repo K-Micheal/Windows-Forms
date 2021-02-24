a2ad063a26f1a14a6cb0cfd018867f0e854e5fe5
Михайло Калюжний <Kaliuzhnyi.M.S@nmu.one>
2021-02-24 18:25:14 +0200

    add comment in User

--- BankApp/User.cs
+++ BankApp/User.cs
@@ -8,6 +8,7 @@ namespace BankApp
 {
     public class User
     {
+        //qwertyqwe
         public int Id { get; set; }
         public string login { get; set; }
         public string password { get; set; }
